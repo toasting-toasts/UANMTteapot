012419feefd0810e7760010436cacca0cdcfb5ad
Toast <tostertonyacount@gmail.com>
2026-02-24 12:38:14 +0200

    Added Header.jsx and styles

---
+++ Newmy-UA/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <p>this is site</p>
+    </>
+  )
+}
+
+export default App;
---
+++ Newmy-UA/src/Header/Header.jsx
@@ -0,0 +1,17 @@
+import {styles} from "/Header.module.scss"
+function Header() {
+
+    return (
+        <header className={styles.header}>
+        <img src="" alt="No image loaded/bad connection"/>
+  
+        <nav>
+          <p>Login</p>
+          <p>Register</p>
+          <p>emptyfornow</p>
+        </nav>
+      </header>
+    )
+  }
+
+export default Header;
